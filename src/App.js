c7cc30547ec087d8e47d5758bf6b6be4fc54a725
kamol-nazarov <kamol.nazarov15@myhunter.cuny.edu>
2020-04-08 00:33:23 -0400

    navbar included

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import AppNavbar from './components/layout/AppNavbar.js';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1> HELLO </h1>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<AppNavbar />
+					<h1>Hello World!</h1>
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
